10a2000cd3a6dbb81765b38f51c1335c2c2984fc
braa16 <Brian@MacBook-Pro-de-Auxi.local>
2024-09-13 12:37:07 +0200

    Delate Code

--- terraform-project-01/modules/virtual-machines/main.tf
+++
@@ -1,9 +0,0 @@
-/*
-resource "azurerm_virtual_machine" "vm" {
-  name = var.vmname
-  location = var.location
-  resource_group_name = var.rgname
-
-
-}
-*/
--- terraform-project-01/modules/virtual-machines/variables.tf
+++
@@ -1,5 +0,0 @@
-
-variable "vmname" {
-  description = var.vmname
-  type        = string
-}
