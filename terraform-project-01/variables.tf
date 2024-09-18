8ecf97fcbc99cfaf4c7671d3804eced1dcc8b965
braa16 <Brian@MacBook-Pro-de-Auxi.local>
2024-09-18 17:32:50 +0200

    Definición de variables

--- terraform-project-01/variables.tf
+++ terraform-project-01/variables.tf
@@ -1,2 +1,15 @@
 # Define las variables de entrada que se utilizara en la configuración para parametrizar valores como nombres de recursos, ubicaciones, etc.
 # Función: Centralizar las variables para una fácil gestión y reutilización.
+
+variable "rgname" {}
+variable "location" {}
+variable "vnetname" {}
+variable "subnetname" {}
+variable "nsgname" {}
+variable "nicname" {}
+variable "vmname" {}
+variable "publicipname" {}
+variable "hddname" {}
+variable "vm_size" {}
+variable "admin_username" {}
+variable "path_ssh" {}
