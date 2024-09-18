e51fdbfe450cc2f9eed6552a34ee1246cec7155c
braa16 <Brian@MacBook-Pro-de-Auxi.local>
2024-09-18 17:27:11 +0200

    Modificación Comentarios

--- terraform-project-01/modules/resource-group/main.tf
+++ terraform-project-01/modules/resource-group/main.tf
@@ -1,4 +1,5 @@
 # Creando Recursos Resource Groups 
+# Un grupo de recursos es una entidad que agrupa todos los recursos relacionados de tu proyecto en Azure, lo que facilita la gestión y administración.
 
 resource "azurerm_resource_group" "rg" {
     location = var.location
