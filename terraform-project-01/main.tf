1b3cab3b781af3d13050c898b61df1afedd7f032
braa16 <Brian@MacBook-Pro-de-Auxi.local>
2024-09-18 17:33:49 +0200

    Module 3 VM

--- terraform-project-01/main.tf
+++ terraform-project-01/main.tf
@@ -1,16 +1,7 @@
 # Este archivo contiene la definición principal de los recursos que se van a crear en Azure. Es la Plantilla Orquestadora 
 # Función: Define la infraestructura específica, como grupos de recursos, redes virtuales, máquinas virtuales, etc.
 
-# Variables de entrada que se utilizara para los modulos que vamos a invocar 
-
-variable "rgname" {}
-variable "location" {}
-variable "vnetname" {}
-variable "subnetname" {}
-variable "nsgname" {}
-variable "nicname" {}
-
-# Crear Resource Group mediante modulos
+# Crear Resource Group.
 
 module "mod_01" { 
     source = "./modules/resource-group"
@@ -18,7 +9,7 @@ module "mod_01" {
     location = var.location
 }
 
-# Crear Recursos Networking (Vnet, Subnet, NSG, NIC) mediante modulos
+# Crear Recursos Networking (Vnet, Subnet, NSG, NIC).
 
  module "mod_02" { 
     source = "./modules/networking"
@@ -28,5 +19,26 @@ module "mod_01" {
     nicname = var.nicname
     rgname = var.rgname
     location = var.location
+    publicipname = var.publicipname
     depends_on = [module.mod_01]
 }
+
+# Crear Recurso VM, acceso SSH e instalacion de nginx.
+
+module "mod_03" {
+    source = "./modules/virtual-machines"
+    vmname = var.vmname
+    rgname = var.rgname
+    location = var.location
+    vnetname = var.vnetname
+    subnetname = var.subnetname
+    nsgname = var.nsgname
+    publicipname = var.publicipname
+    nicname = var.nicname
+    admin_username = var.admin_username
+    vm_size = var.vm_size
+    hddname = var.hddname
+    path_ssh = var.path_ssh
+    depends_on = [module.mod_02]
+}
+
